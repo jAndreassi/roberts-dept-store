d63e48305fe476f1d31be753aeae613b3012b1d8
jake rudlong <jrudlong@gmail.com>
2023-05-04 21:53:32 -0500

    adds react-router-dom and routes, changes class to className

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,19 +3,22 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
 
 function BasicExample() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#">
-          <span class="d-none d-sm-block">Robert's Department Store</span>
-          <span class="d-block d-sm-none">Robert's Dept. Store</span>
+        <Link to="/">
+        <Navbar.Brand>
+          <span className="d-none d-sm-block">Robert's Department Store</span>
+          <span className="d-block d-sm-none">Robert's Dept. Store</span>
         </Navbar.Brand>
+        </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Footwear</Nav.Link>
+            <Nav.Link href="">Footwear</Nav.Link>
             <Nav.Link href="#link">Clothing</Nav.Link>
             <Nav.Link href="#link">Accessories</Nav.Link>
           </Nav>

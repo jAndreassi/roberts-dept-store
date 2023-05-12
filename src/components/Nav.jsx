dbb57ff869a6d5789b2219a4bc7ec6847c7c0c07
Jared A <jroberts1931@gmail.com>
2023-05-12 15:24:47 -0400

    fixed nav and added search bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,14 +3,17 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
+import "../assets/css/nav.css";
 
 function BasicExample() {
   return (
     <>
       <Navbar
         style={{
-          backgroundColor: "white",
+          backgroundColor: "#292827",
           fontFamily: "Inter",
           fontSize: "1.2em",
         }}
@@ -21,9 +24,12 @@ function BasicExample() {
             style={{
               textDecoration: "none",
               marginRight: "2rem",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
             }}>
             <Navbar.Brand>
-              <span className="d-none d-sm-block">
+              <span className="d-none d-sm-block" style={{ color: "white" }}>
                 Robert's Department Store
                 <br />
                 <small>Since 1931</small>
@@ -32,12 +38,21 @@ function BasicExample() {
             </Navbar.Brand>
           </Link>
         </Container>
+        <Form className="d-flex" style={{ marginRight: "1em" }}>
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-light">Search</Button>
+        </Form>
       </Navbar>
       <Navbar
         style={{
           backgroundColor: "#D8DBDE",
           fontFamily: "Inter",
-          fontSize: "1.2em",
+          fontSize: "1em",
         }}
         expand="lg">
         <Container>
@@ -54,6 +69,10 @@ function BasicExample() {
 
                   marginRight: "1rem",
                   color: "black",
+                }}
+                activeStyle={{
+                  fontWeight: "bold",
+                  color: "blue",
                 }}>
                 Footwear
               </Link>
@@ -87,7 +106,27 @@ function BasicExample() {
                   marginRight: "1rem",
                   color: "black",
                 }}>
+                Hi-Vis Clothing
+              </Link>
+              <Link
+                to="/Brands"
+                style={{
+                  textDecoration: "none",
+                  fontSize: "1em",
+                  marginRight: "1rem",
+                  color: "black",
+                }}>
                 Brands
+              </Link>
+              <Link
+                to="/Brands"
+                style={{
+                  textDecoration: "none",
+                  fontSize: "1em",
+                  marginRight: "1rem",
+                  color: "black",
+                }}>
+                Store Info
               </Link>
             </Nav>
           </Navbar.Collapse>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,23 +5,32 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Wolbanner from "../assets/images/WOLVERINE.BANNER.png";
 import boot1 from "../assets/images/W03226.jpg";
+import boot2 from "../assets/images/W10315.jpg";
+import boot3 from "../assets/images/W10317.jpg";
+import boot4 from "../assets/images/W10788.jpg";
+import Carousel from "react-bootstrap/Carousel";
+import Slide1 from "../assets/images/brandslide1.png";
+import Slide2 from "../assets/images/brandslide2.png";
+import Slide3 from "../assets/images/brandslide3.png";
+
+const boots = [boot1, boot2, boot3, boot4];
 
 function BasicExample() {
   return (
-    <div style={{ paddingTop: "2em", paddingBottom: "10em" }}>
-      <Card className="border-0">
+    <div style={{ paddingBottom: "10em" }}>
+      <Card className="border-0" style={{ paddingBottom: "5em" }}>
         <Card.Img
           variant="top"
           src={hero}
           style={{
             width: "100%",
-            height: "500px",
+            height: "350px",
             objectFit: "cover",
-            objectPosition: "center 70%",
+            objectPosition: "center 60%",
           }}
         />
-        <Card.Body>
-          <Card.Text>
+        {/* <Card.Body>
+          <Card.Text style={{ fontFamily: "Inter" }}>
             Roberts is a local business with a rich history, having been
             established in 1931 and remaining under the ownership of only two
             families since then. As a leading retailer of men's clothing and
@@ -38,8 +47,56 @@ function BasicExample() {
             serving generations of families with their clothing and footwear
             needs.
           </Card.Text>
-        </Card.Body>
+        </Card.Body> */}
       </Card>
+
+      <Carousel
+        style={{
+          border: "5px solid black",
+          borderRadius: "50px",
+          marginRight: "3em",
+          marginLeft: "3em",
+        }}>
+        <Carousel.Item interval={2000}>
+          <img
+            className="d-block w-100"
+            src={Slide1}
+            alt="First slide"
+            style={{ height: "400px" }}
+          />
+          <Carousel.Caption style={{ color: "black" }}>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={2000}>
+          <img
+            className="d-block w-100"
+            src={Slide2}
+            alt="Second slide"
+            style={{ height: "400px" }}
+          />
+          <Carousel.Caption style={{ color: "black" }}>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={2000}>
+          <img
+            className="d-block w-100"
+            src={Slide3}
+            alt="Third slide"
+            style={{ height: "400px" }}
+          />
+          <Carousel.Caption style={{ color: "black" }}>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
       <Card className="bg-dark text-white">
         <Card.Img src={Wolbanner} alt="Card image" />
         <Card.ImgOverlay>
@@ -63,8 +120,8 @@ function BasicExample() {
         </Card.ImgOverlay>
       </Card>
       <div style={{ paddingTop: "2em" }}>
-        <Card>
-          <Card.Body>
+        <Card className="border-0">
+          <Card.Body style={{ fontFamily: "Inter" }}>
             At Roberts, we take great pride in offering our customers only the
             best quality brands. Our selection includes some of the most
             reputable names in the industry such as Levis, Khul, Carhartt,
@@ -85,7 +142,7 @@ function BasicExample() {
         {Array.from({ length: 4 }).map((_, idx) => (
           <Col key={idx}>
             <Card>
-              <Card.Img variant="top" src={boot1} />
+              <Card.Img variant="top" src={boots[idx]} />
               <Card.Body>
                 <Card.Title>Card title</Card.Title>
                 <Card.Text>
